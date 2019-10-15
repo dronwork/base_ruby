fec3db9f1f25a13aa4e481f38dfea060e4774765
ANDREI KEIDA <dronwork.ru@outlook.com>
2019-10-15 14:03:34 +0300

    correction v.3

--- Task_1/quadratic_equation.rb
+++ Task_1/quadratic_equation.rb
@@ -1,24 +1,19 @@
-class QuadEquation
-  puts "Enter coefficient а:"
-  a = gets.to_f
-  puts "Enter coefficient b:"
-  b = gets.to_f
-  puts "Enter coefficient c:"
-  c = gets.to_f
+puts 'Enter coefficient а:'
+a = gets.to_f
+puts 'Enter coefficient b:'
+b = gets.to_f
+puts 'Enter coefficient c:'
+c = gets.to_f
 
-  def self.count_func(a, b, c)
-    d = b**2 - 4 * a * c
-  if d < 0
-    puts "Discriminant: #{d}, no roots"
-  elsif d == 0
-    x = -b / (2 * a)
-    puts "Discriminant: #{d}, roots are equal: #{x}"
-  else
-    e = Math.sqrt(d)
-    x1 = (-b + e) / (2 * a)
-    x2 = (-b - e) / (2 * a)
-    puts "Discriminant: #{d}, roots of the equation: #{x1},#{x2}"
-  end
-  end
-  QuadEquation.count_func(a, b, c)
+d = b**2 - 4 * a * c
+if d < 0
+  puts "Discriminant: #{d}, no roots"
+elsif d == 0
+  x = -b / (2 * a)
+  puts "Discriminant: #{d}, roots are equal: #{x}"
+else
+  e = Math.sqrt(d)
+  x1 = (-b + e) / (2 * a)
+  x2 = (-b - e) / (2 * a)
+  puts "Discriminant: #{d}, roots of the equation: #{x1},#{x2}"
 end
--- Task_1/right_triangle.rb
+++ Task_1/right_triangle.rb
@@ -1,24 +1,19 @@
-class RightTriangle
-  sides_triangle = [] # Array
-  puts 'Enter side A:'
-  sides_triangle << gets.chomp.to_f
-  puts 'Enter side B:'
-  sides_triangle << gets.chomp.to_f
-  puts 'Enter side C:'
-  sides_triangle << gets.chomp.to_f
-  a, b, c = sides_triangle.sort!
+sides_triangle = [] # Array
+puts 'Enter side A:'
+sides_triangle << gets.chomp.to_f
+puts 'Enter side B:'
+sides_triangle << gets.chomp.to_f
+puts 'Enter side C:'
+sides_triangle << gets.chomp.to_f
+a, b, c = sides_triangle.sort!
 
-  def self.right_triangle(a, b, c)
-  if a**2 + b**2 == c**2
-    puts 'Your triangle is rectangular !'
-  else
-    puts 'Your triangle is not rectangular'
+if a**2 + b**2 == c**2
+  puts 'Your triangle is rectangular !'
+else
+  puts 'Your triangle is not rectangular'
 end
-  if a == b && b == c
-    puts 'Your triangle is isosceles and equilateral, but not rectangular'
-  elsif a == b || a == c || b == c
-    puts 'Your triangle isosceles'
-  end
-  end
-  RightTriangle.right_triangle(a, b, c)
+if a == b && b == c
+  puts 'Your triangle is isosceles and equilateral, but not rectangular'
+elsif a == b || a == c || b == c
+  puts 'Your triangle isosceles'
 end
